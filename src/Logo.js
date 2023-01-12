75cf6c89d1ada28a0f50527affa796afdd5afc5b
cpk0709 <cpk0709@gmail.com>
2023-01-12 22:05:02 +0900

    .

--- src/Logo.js
+++ src/Logo.js
@@ -3,7 +3,7 @@ import {Image} from 'react-native';
 import logo from './asset/pics/home.png';
 
 const Logo = () => {
-  return <Image style={{width: 40, height: 40}} source={logo} />;
+  return <Image style={{width: 35, height: 35}} source={logo} />;
 };
 
 export default Logo;
