c1d6230b930583b106253d7d587ff37b4a3c1bdb
cpk0709 <cpk0709@gmail.com>
2023-01-13 21:56:28 +0900

    reset Project

--- src/Logo.js
+++
@@ -1,9 +0,0 @@
-import {Image} from 'react-native';
-
-import logo from './asset/pics/home.png';
-
-const Logo = () => {
-  return <Image style={{width: 35, height: 35}} source={logo} />;
-};
-
-export default Logo;
