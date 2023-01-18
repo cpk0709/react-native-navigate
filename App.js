501c6132c03748a14ba302259fc63822c6f4c081
cpk0709 <cpk0709@actbase.io>
2023-01-18 23:59:07 +0900

    달력 컴포넌트 나눔

--- App.js
+++ App.js
@@ -15,41 +15,7 @@ import Margin from './src/Margin';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import {useCalendar} from './src/hook/useCalendar';
 import {useTodoList} from './src/hook/useTodoList';
-
-const columnSize = 35;
-
-const Column = ({
-  text,
-  color,
-  opacity,
-  disabled = false,
-  onPress,
-  isSelected,
-}) => {
-  return (
-    <TouchableOpacity
-      onPress={onPress}
-      disabled={disabled}
-      style={{
-        width: columnSize,
-        height: columnSize,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: isSelected ? '#c2c2c2' : 'transparent',
-        borderRadius: columnSize / 2,
-      }}>
-      <Text style={[styles.day, {color, opacity}]}>{text}</Text>
-    </TouchableOpacity>
-  );
-};
-
-const ArrowButton = ({onPress, text}) => {
-  return (
-    <TouchableOpacity style={{padding: 10}} onPress={onPress}>
-      <Text style={{color: '#404040'}}>{text}</Text>
-    </TouchableOpacity>
-  );
-};
+import Calendar from './src/Calendar';
 
 const App = () => {
   const now = dayjs();
@@ -74,80 +40,21 @@ const App = () => {
 
   const onPressRightButton = add1Month;
 
-  const ListHeaderComponent = () => {
-    const currentDateText = dayjs(selectedDate).format('YYYY.MM.DD.');
-    return (
-      <View>
-        <Margin height={15} />
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <ArrowButton text={'Prev'} onPress={onPressLeftButton} />
-          <TouchableOpacity onPress={() => showDatePicker()}>
-            <Text style={{fontSize: 20, color: '#404040'}}>
-              {currentDateText}
-            </Text>
-          </TouchableOpacity>
-          <ArrowButton text={'Next'} onPress={onPressRightButton} />
-        </View>
-        <Margin height={15} />
-        <View style={{flexDirection: 'row'}}>
-          {[0, 1, 2, 3, 4, 5, 6].map(day => {
-            const dayText = getDayText(day);
-            const color = getDayColor(day);
-            return (
-              <Column
-                disabled
-                key={`day-${day}`}
-                text={dayText}
-                color={color}
-                opacity={1}
-              />
-            );
-          })}
-        </View>
-      </View>
-    );
-  };
-
-  const renderItem = ({item: date}) => {
-    const dateText = dayjs(date).get('date');
-    const day = dayjs(date).get('day');
-    const color = getDayColor(day);
-    const isCurrentMonth = dayjs(date).isSame(selectedDate, 'month');
-    const onPress = () => {
-      setSelectedDate(date);
-    };
-    const isSelected = dayjs(date).isSame(selectedDate, 'date');
-    return (
-      <Column
-        text={dateText}
-        color={color}
-        opacity={isCurrentMonth ? 1 : 0.3}
-        onPress={onPress}
-        isSelected={isSelected}
-      />
-    );
-  };
-
   return (
     <View style={[styles.container]}>
       <Image
         source={require('./src/assets/background_01.jpeg')}
         style={{width: '100%', height: '100%', position: 'absolute'}}
       />
-      <FlatList
-        data={columns}
-        keyExtractor={(_, index) => `column-${index}`}
-        renderItem={renderItem}
-        numColumns={7}
-        ListHeaderComponent={ListHeaderComponent}
-        style={{paddingTop: 50}}
-      />
 
+      <Calendar
+        columns={columns}
+        selectedDate={selectedDate}
+        onPressLeftButton={onPressLeftButton}
+        onPressRightButton={onPressRightButton}
+        setSelectedDate={setSelectedDate}
+        showDatePicker={showDatePicker}
+      />
       <FlatList
         data={todoList}
         renderItem={({item: todo}) => {
@@ -174,9 +81,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  day: {
-    paddingHorizontal: 3,
   },
 });
 
